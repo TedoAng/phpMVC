7758edfa5a88b69d267d0bd36106e02fa03af550
Teodor Angelov <teodor.angelov@fastcomet.com>
2023-10-10 18:02:38 +0300

    hello

--- gf/SourceFiles/Routers/DefaultRouter.php
+++ gf/SourceFiles/Routers/DefaultRouter.php
@@ -4,6 +4,8 @@ namespace GF\Routers;
 
 class DefaultRouter implements IRouter
 {
-    
-    
+    public function getURI()
+    {
+        return substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME']) + 1);
+    }
 } 
